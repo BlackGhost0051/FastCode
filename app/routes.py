0caf36e49857537c32852b93070c35cb7ef035a2
Black Ghost <147267828+BlackGhost0051@users.noreply.github.com>
2024-09-17 20:36:59 +0200

    Add dynamic list

--- app/routes.py
+++ app/routes.py
@@ -8,12 +8,15 @@ def home():
 @current_app.route('/python')
 def python():
     return render_template('/languages/python/main.html')
-@current_app.route('/python/list')
-def python_list():
-    python_folder = os.path.join(current_app.root_path + '/static/languages/python')
-    files = os.listdir(python_folder)
-    return jsonify(files)
 
 @current_app.route('/java')
 def java():
     return  render_template('/languages/java/main.html')
+@current_app.route('/<language>/list')
+def send_list(language):
+    language_folder = os.path.join(current_app.root_path + '/static/languages/' + language)
+    try:
+        files = os.listdir(language_folder)
+        return jsonify(files) , 200
+    except FileNotFoundError:
+        return jsonify({"error": "Folder not found"}), 404
