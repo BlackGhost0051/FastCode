9375ee811fd30078f38b2eccae11ce7c1485e825
Black Ghost <147267828+BlackGhost0051@users.noreply.github.com>
2024-09-24 08:06:48 +0200

    Add code_typing

--- app/static/js/typing.js
+++ app/static/js/typing.js
@@ -1,4 +1,54 @@
+var typingIndex = 0;
+
 var keyboard = document.getElementById("keyboard");
+var code_typing = document.getElementById("code_typing");
+
+
+const urlParams = new URLSearchParams(window.location.search);
+
+const file = urlParams.get('file');
+const language = urlParams.get('language');
+
+console.log(file + " " + language);
+
+var codeTaskUrl = "/" + language + "/get_code/" + file;
+var code;
+
+fetch(codeTaskUrl)
+.then(response => {
+    if(response.ok){
+        return response.text();
+    } else {
+        throw new Error("File not found");
+    }
+})
+.then(code=> {
+    console.log(code);
+
+    code_typing.innerHTML = '';
+
+    for(let i = 0; i < code.length; i++){
+        let char = code[i];
+        let span = document.createElement("span");
+
+        span.textContent = char;
+        span.classList.add("code-char");
+        code_typing.appendChild(span);
+    }
+
+
+})
+.catch(error => {
+    console.error('Error:', error);
+});
+
+
+
+
+function typing(){
+
+}
+
 
 
 function keyDown(event) {
@@ -9,7 +59,7 @@ function keyDown(event) {
     
     if (keyElement) {
         keyElement.classList.add('active');
-        event.preventDefault();
+        //event.preventDefault();
     }
 }
 
