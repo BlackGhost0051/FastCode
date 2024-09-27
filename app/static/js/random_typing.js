30aff772082bf4ae89cd2e76cbfed8704bd38e3a
Black Ghost <147267828+BlackGhost0051@users.noreply.github.com>
2024-09-27 07:50:46 +0200

    Add random button

--- app/static/js/random_typing.js
+++ app/static/js/random_typing.js
@@ -2,10 +2,38 @@ var random_button = document.getElementById("random_button");
 
 
 random_button.addEventListener('click', () => {
-    // get /languages_list lenght ( make random )
-    // random = 2 | 2 = java
-    // java/list get lenght 
-    // make random for files in java
-    // create url /typing?...java&...file.java
-    // GO to url
+
+    fetch("/languages_list")
+        .then(responce => {
+            if(responce.ok){
+                return responce.json();
+            } else {
+                throw new Error("Something go wrong");
+            }
+        })
+        .then(data =>{
+            const keys = Object.keys(data);
+            const randomLanguage = data[keys[Math.floor(Math.random() * keys.length)]];
+
+            
+            return fetch(`/${randomLanguage}/list`)
+                .then(response => {
+                    if(response.ok){
+                        return response.json();
+                    } else {
+                        throw new Error("Failed to get files for the selected language");
+                    }
+                })
+                .then(files =>{
+                    const randomFile = files[Math.floor(Math.random() * files.length)];
+                    console.log(randomLanguage + " " + randomFile);
+
+
+                    const url = `/typing?file=${randomFile}&language=${randomLanguage}`;
+                    window.location.href = url;
+                });
+        })
+        .catch(error => {
+            console.error(error);
+        });
 });
