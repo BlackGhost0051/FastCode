104ee6b97c98d615c0aa61ee0ba5422138c95e56
Black Ghost <147267828+BlackGhost0051@users.noreply.github.com>
2024-10-03 12:59:12 +0200

    Add animation

--- app/static/js/error.js
+++ app/static/js/error.js
@@ -1,1 +1,34 @@
-console.log("TEST");
+const statusCodeElement = document.getElementById("status_code");
+const body = document.body;
+const statusText = statusCodeElement.textContent;
+
+let currentIndex = 0;
+let delay = 2000;
+let hue = 0;
+
+statusCodeElement.innerHTML = statusText.split('').map(char => `<span>${char}</span>`).join('');
+const spans = document.querySelectorAll("#status_code span");
+
+
+
+function animate() {
+    spans.forEach((span, index) => {
+        setTimeout(() => {
+            span.style.opacity = 0;
+            setTimeout(() => {
+                span.style.opacity = 1;
+            }, delay/2);
+        }, index * delay);
+    });
+}
+
+setInterval(animate, spans.length * delay);
+animate();
+
+
+function changeColor() {
+    hue = (hue + 1) % 360;
+    body.style.backgroundColor = `hsl(${hue}, 100%, 50%)`;
+}
+
+setInterval(changeColor, 100);
