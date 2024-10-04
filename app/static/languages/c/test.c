3b611b1634bfd17d0fe673c7ec278404fc037960
Black Ghost <147267828+BlackGhost0051@users.noreply.github.com>
2024-10-04 18:11:03 +0200

    Mod code for scroll

--- app/static/languages/c/test.c
+++ app/static/languages/c/test.c
@@ -4,3 +4,45 @@ int main(){
     printf("Test");
     return 0;
 }
+
+#include <stdio.h>
+
+int main(){
+    printf("Test");
+    return 0;
+}
+
+#include <stdio.h>
+
+int main(){
+    printf("Test");
+    return 0;
+}
+
+#include <stdio.h>
+
+int main(){
+    printf("Test");
+    return 0;
+}
+
+#include <stdio.h>
+
+int main(){
+    printf("Test");
+    return 0;
+}
+
+#include <stdio.h>
+
+int main(){
+    printf("Test");
+    return 0;
+}
+
+#include <stdio.h>
+
+int main(){
+    printf("Test");
+    return 0;
+}
