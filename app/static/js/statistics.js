c4dd92210472c41487562da6a8cc614f9b7e37f2
Black Ghost <147267828+BlackGhost0051@users.noreply.github.com>
2024-10-11 12:19:32 +0200

    Add js

--- app/static/js/statistics.js
+++ app/static/js/statistics.js
@@ -1,1 +1,24 @@
 console.log("Test");
+
+let info_div = document.getElementById("info_div");
+
+fetch("/get_statistics")
+    .then(responce => {
+        if(responce.ok){
+            return responce.json();
+        } else {
+            throw new Error("Something go wrong");
+        }
+    })
+    .then(data => {
+        const keys = Object.keys(data);
+        
+        
+        console.log(keys);
+        console.log(data);
+        console.log(data[0].file_name);
+        
+    })
+    .catch(error => {
+        console.log(error);
+    })
