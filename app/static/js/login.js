a7804cbc753f4877aa2af191c48f9b96a23f97f1
Black Ghost <147267828+BlackGhost0051@users.noreply.github.com>
2024-12-27 15:58:33 +0100

    Add send POST

--- app/static/js/login.js
+++ app/static/js/login.js
@@ -1,4 +1,35 @@
 console.log("Login");
 
 
-//const login_button = document.getElementById("login_button");
+const login_button = document.getElementById("login_button");
+const login_input = document.getElementById("login");
+const password_input = document.getElementById("password");
+
+
+
+login_button.addEventListener("click", () => {
+    fetch("/login",{
+        method: "POST",
+        body: JSON.stringify(
+            {
+            login: login_input.value,
+            password: password_input.value
+            }
+        ),
+        headers:{
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    })
+    .then((response) => { 
+        if (!response.ok){
+            console.error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+    })
+    .then((data) => {
+        console.log(data)
+    })
+    .catch((error) => {
+        console.error("Error occurred:", error);
+    });
+});
