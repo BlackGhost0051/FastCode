643fe1494e52387189d39894097ae9c73192c25d
Black Ghost <147267828+BlackGhost0051@users.noreply.github.com>
2024-12-27 16:04:43 +0100

    Add send POST

--- app/static/js/register.js
+++ app/static/js/register.js
@@ -1,3 +1,34 @@
 console.log("Register");
 
-//const register_button = document.getElementById("register_button");
+const register_button = document.getElementById("register_button");
+const login_input = document.getElementById("login");
+const password_0_input = document.getElementById("password_0");
+const password_1_input = document.getElementById("password_1");
+
+register_button.addEventListener("click", () => {
+    fetch("/register",{
+        method: "POST",
+        body: JSON.stringify(
+            {
+            login: login_input.value,
+            password_0: password_0_input.value,
+            password_1: password_1_input.value
+            }
+        ),
+        headers:{
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    })
+    .then((response) => { 
+        if (!response.ok){
+            console.error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+    })
+    .then((data) => {
+        console.log(data)
+    })
+    .catch((error) => {
+        console.error("Error occurred:", error);
+    });
+});
