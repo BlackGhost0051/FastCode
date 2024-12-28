5ab0cd82fb8d2a0fdd9cf12d5876b5cd639f3894
Black Ghost <147267828+BlackGhost0051@users.noreply.github.com>
2024-12-28 15:52:30 +0100

    Add make_hash, verify_password

--- app/Managers/CryptoManager.py
+++ app/Managers/CryptoManager.py
@@ -1,8 +1,22 @@
-import crypt
+import hashlib
 
 class CryptoManager:
     def __init__(self):
         self
 
-    def make_hash(self):
-        self
+    def make_hash(self, password: str, salt: str = None) -> str:
+        if not salt:
+            salt = hashlib.sha3_512(os.urandom(32)).hexdigest()[:64]
+        salted_password = salt + password
+        hashed = hashlib.sha3_512(salted_password.encode()).hexdigest()
+        return f"{salt}${hashed}"
+
+    def verify_password(self, password: str, hashed_password: str) -> bool:
+        try:
+            salt, stored_hash = hashed_password.split('$')
+            salted_password = salt + password
+            computed_hash = hashlib.sha3_512(salted_password.encode()).hexdigest()
+            return computed_hash == stored_hash
+        except ValueError:
+            raise ValueError("Invalid hashed password format")
+
