7861e5ba5cd548181bec5150e802b23f429f6909
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-01-28 19:42:56 +0100

    Mod LanguagesManager.py

---
+++ app/Managers/LanguagesManager.py
@@ -0,0 +1,16 @@
+import os
+
+EXTENSIONS_PATH = {
+    '.py': 'python',
+    '.java': 'java',
+    '.c': 'c',
+    '.cpp': 'c++',
+}
+
+
+class LanguagesManager:
+
+
+    def __init__(self):
+        pass
+    # verify languages folders by list
