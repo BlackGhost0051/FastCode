ae1064961dfc2a833bf23c627d39815c41a85dae
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-02-12 12:14:01 +0100

    Mod profile.js

--- app/static/js/profile.js
+++ app/static/js/profile.js
@@ -1,1 +1,5 @@
-console.log("Profile");
+const user_links_list = document.getElementById("user_links_list");
+const user_detail_info = document.getElementById("user_detail_info");
+const profile_image = document.getElementById("profile_image");
+
+
